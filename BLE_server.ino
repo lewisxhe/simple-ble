0af20ecedfbb179a7fb7cdcd609feaca424306d6
lewisxhe <lewishe@outlook.com>
2019-04-23 10:41:47 +0800

    add dht sensor service and led ctrl

--- BLE_server.ino
+++ BLE_server.ino
@@ -8,9 +8,18 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+#include <DHT.h>
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
+
+#define BOOT_PIN        0
+#define LED_PIN         16
+#define DTH_SENSOR_PIN  22
+#define ADC_PIN         32
+#define DHTTYPE DHT11 // DHT 22  (AM2302), AM2321
+DHT dht(DTH_SENSOR_PIN, DHTTYPE);
+
 
 #define CTRL_SERVICE_UUID                   "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CTRL_CHARACTERISTIC_UUID            "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -20,29 +29,46 @@
 
 static BLECharacteristic *pLEDControlCharacteristic = nullptr;
 static BLECharacteristic *pSensorCharacteristic = nullptr;
+static BLEDescriptor  *pSensorDescriptor = nullptr;
+
 static bool deviceConnected = false;
-uint32_t value = 0;
+static bool enableNotify = false;
+
+class SensorDescriptorCallbacks: public BLEDescriptorCallbacks
+{
+    void onWrite(BLEDescriptor *pDescriptor)
+    {
+        uint8_t *value = pDescriptor->getValue();
+        Serial.print("SensorDescriptorCallbacks:");
+        if (value) {
+            enableNotify = value ? true : false;
+            Serial.println(value[0]);
+        }
+    }
+    void onRead(BLEDescriptor *pDescriptor)
+    {
+
+    }
+};
+
 
 class LEDControlCallbacks: public BLECharacteristicCallbacks
 {
-    void onWrite(BLECharacteristic *pLEDControlCharacteristic)
+    void onWrite(BLECharacteristic *pCharacteristic)
     {
-        std::string value = pLEDControlCharacteristic->getValue();
-        Serial.println(pLEDControlCharacteristic->getValue().c_str());
+        Serial.print("LEDControlCallbacks Write-->");
+        std::string value = pCharacteristic->getValue();
+        Serial.println(pCharacteristic->getValue().c_str());
 
-        // if (value.length() > 0) {
-        //     Serial.println("*********");
-        //     Serial.print("New value: ");
-        //     for (int i = 0; i < value.length(); i++)
-        //         Serial.print(value[i]);
-
-        //     Serial.println();
-        //     Serial.println("*********");
-        // }
+        uint8_t *data = pCharacteristic->getData();
+        if (data)
+            digitalWrite(LED_PIN, data[0]);
     }
-    void onRead(BLECharacteristic *pLEDControlCharacteristic)
+    void onRead(BLECharacteristic *pCharacteristic)
     {
-
+        Serial.println("LEDControlCallbacks Read-->");
+        uint8_t data = digitalRead(LED_PIN);
+        pCharacteristic->setValue(&data, 1);
     }
 };
 
@@ -57,6 +83,7 @@ class DeviceServerCallbacks: public BLEServerCallbacks
     void onDisconnect(BLEServer *pServer)
     {
         deviceConnected = false;
+        enableNotify = false;
         Serial.println("Device disconnect");
     }
 };
@@ -66,6 +93,10 @@ void setup()
 {
     Serial.begin(115200);
     Serial.println("Starting BLE work!");
+
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
+
     BLEDevice::init("BLE Testing");
     BLEServer *pServer = BLEDevice::createServer();
     pServer->setCallbacks(new DeviceServerCallbacks());
@@ -74,12 +105,16 @@ void setup()
     BLEService *pLEDControlService = pServer->createService(CTRL_SERVICE_UUID);
 
     pLEDControlCharacteristic = pLEDControlService->createCharacteristic(
-                          CTRL_CHARACTERISTIC_UUID,
-                          BLECharacteristic::PROPERTY_READ |
-                          BLECharacteristic::PROPERTY_WRITE
-                      );
+                                    CTRL_CHARACTERISTIC_UUID,
+                                    BLECharacteristic::PROPERTY_READ |
+                                    BLECharacteristic::PROPERTY_WRITE
+                                );
 
     pLEDControlCharacteristic->setCallbacks(new LEDControlCallbacks());
+
+    //Setting init value
+    uint8_t data = digitalRead(LED_PIN);
+    pLEDControlCharacteristic->setValue(&data, 1);
 
     /*Sensor Service*/
     BLEService *pSensorService = pServer->createService(SENSOR_SERVICE_UUID);
@@ -93,6 +128,10 @@ void setup()
 
 
     pSensorCharacteristic->addDescriptor(new BLE2902());
+
+    pSensorDescriptor = pSensorCharacteristic->getDescriptorByUUID("2902");
+
+    pSensorDescriptor->setCallbacks(new SensorDescriptorCallbacks());
 
 
     pSensorService->start();
@@ -108,15 +147,20 @@ void setup()
     Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
+uint8_t sensorArray[12];
 void loop()
 {
     if (deviceConnected) {
-        BLEDescriptor  *des = pSensorCharacteristic->getDescriptorByUUID("2902");
-        uint8_t *data = des->getValue();
-        if (data) {
-            pSensorCharacteristic->setValue((uint8_t *)&value, 4);
+        if (enableNotify) {
+            *(float *)&(sensorArray[0]) = dht.readHumidity();
+            *(float *)&(sensorArray[4]) = dht.readTemperature();
+            *(int *)&(sensorArray[9]) = map(analogRead(ADC_PIN), 0, 4096, 100, 0);
+            pSensorCharacteristic->setValue((uint8_t *)sensorArray, sizeof(sensorArray) / sizeof(sensorArray[0]));
             pSensorCharacteristic->notify();
-            value++;
         }
-        delay(2000);
     }
+
+    // esp_light_sleep_start();
+
+    delay(1000);
+}
